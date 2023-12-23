7b6180f0d90fbca30192131b2af4bd31f82a66a7
stanoz <s.oziemczuk@gmail.com>
2023-12-23 10:03:53 +0100

    catching backend exceptions on frontend

--- mpr_frontend/src/main/java/com/example/mpr_frontend/exception_handlers/PersonExceptionHandler.java
+++ mpr_frontend/src/main/java/com/example/mpr_frontend/exception_handlers/PersonExceptionHandler.java
@@ -4,22 +4,29 @@ import com.example.mpr_frontend.exceptions.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class PersonExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(PersonNotFoundException.class)
-    protected ResponseEntity<String> handleNotFound(RuntimeException ex, WebRequest request){
-        return ResponseEntity.notFound().build();
-    }
-    @ExceptionHandler(PersonAlreadyExistException.class)
-    protected ResponseEntity<String> handleAlreadyExist(RuntimeException ex, WebRequest request){
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-    @ExceptionHandler({InvalidPersonAgeException.class, InvalidPersonEmailException.class,
-            InvalidPersonLoginException.class, InvalidPersonNameException.class, InvalidPersonPasswordException.class})
-    protected ResponseEntity<String> handleInvalidArgument(RuntimeException ex, WebRequest request){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+//    @ExceptionHandler(PersonNotFoundException.class)
+//    protected ResponseEntity<String> handleNotFound(RuntimeException ex, WebRequest request){
+//        return ResponseEntity.notFound().build();
+//    }
+//    @ExceptionHandler(PersonAlreadyExistException.class)
+//    protected ResponseEntity<String> handleAlreadyExist(RuntimeException ex, WebRequest request){
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+//    @ExceptionHandler({InvalidPersonAgeException.class, InvalidPersonEmailException.class,
+//            InvalidPersonLoginException.class, InvalidPersonNameException.class, InvalidPersonPasswordException.class})
+//    protected ResponseEntity<String> handleInvalidArgument(RuntimeException ex, WebRequest request){
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected String handleExceptionFromBackend(HttpClientErrorException ex, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("Error", ex.getMessage());
+        return ("redirect:index");
     }
 }
