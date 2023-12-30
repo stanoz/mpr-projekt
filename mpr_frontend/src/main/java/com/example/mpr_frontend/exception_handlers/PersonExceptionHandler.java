0a49de26cfea3c9c984419c9baeffbf7ee1f2e35
stanoz <s.oziemczuk@gmail.com>
2023-12-30 14:08:10 +0100

    logger for exceptions

--- mpr_frontend/src/main/java/com/example/mpr_frontend/exception_handlers/PersonExceptionHandler.java
+++ mpr_frontend/src/main/java/com/example/mpr_frontend/exception_handlers/PersonExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.mpr_frontend.exception_handlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,8 +12,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class PersonExceptionHandler extends ResponseEntityExceptionHandler {
+    private static Logger logger = LoggerFactory.getLogger(PersonExceptionHandler.class);
     @ExceptionHandler(Exception.class)
     protected String handleExceptionFromBackend(HttpClientErrorException ex, RedirectAttributes redirectAttributes) {
+        logger.info(ex.getMessage());
         String errorMessage = ex.getMessage()
                 .replaceAll("^\\s*\\d{3}\\s*\\w+\\s*\\w+:\\s*", "");
         redirectAttributes.addAttribute("Error",
